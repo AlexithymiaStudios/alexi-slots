cf248e4b2a43e54346e2ffb48bdfc7b932d88d30
J Cheyo Jimenez <cheyo@masters3d.com>
2017-06-26 15:45:25 -0700

    added forms to options page

--- scripts/options-app.js
+++ scripts/options-app.js
@@ -1,27 +1,33 @@
 'use strict';
 
-var form = document.createElement('form');
-var label = document.createElement('label');
-var selectUser = document.getElementById('selectUser');
-var playButton = document.getElementById('startPlaying');
-var currentOptionShowing = 'newUser';
-
-if (Data.loadCurrentUser() === null) {
+function insertNewUser() {
+  var form = document.createElement('form');
+  var label = document.createElement('label');
+  var selectUser = document.getElementById('selectUser');
+  var button = document.createElement('button');
   form.setAttribute('id', 'newUser');
-  currentOptionShowing = 'newUser';
   var input = document.createElement('input');
   input.setAttribute('type', 'text');
-  input.setAttribute('name', 'userName');
+  input.setAttribute('name', 'userNameSelected');
   input.setAttribute('value', 'Enter your name');
   label.textContent = 'New User';
   form.appendChild(label);
   form.appendChild(input);
+  button.setAttribute('id', 'addNewUser');
+  button.textContent = 'Start Playing';
+  form.appendChild(button);
   selectUser.appendChild(form);
-} else if (Data.getAllUsers().length > 0) {
+}
+
+function insertPastUsers() {
+  var form = document.createElement('form');
+  var label = document.createElement('label');
+  var selectUser = document.getElementById('selectUser');
+  var button = document.createElement('button');
   form.setAttribute('id', 'pastUsers');
-  currentOptionShowing = 'pastUsers';
   label.textContent = 'Select the User';
   var select = document.createElement('select');
+  select.setAttribute('name', 'userNameSelected');
   var allusers = Data.getAllUsers();
   for(var i = 0; i < allusers.length; i++){
     var each = allusers[i];
@@ -32,10 +38,43 @@ if (Data.loadCurrentUser() === null) {
   }
   form.appendChild(label);
   form.appendChild(select);
+  button.setAttribute('id', 'showPastUsers');
+  button.textContent = 'Play';
+  form.appendChild(button);
   selectUser.appendChild(form);
-}
+};
 
-playButton.addEventListener('click', function(event){
-  var formShowing = document.getElementById(currentOptionShowing);
-  console.log(formShowing);
+insertPastUsers();
+insertNewUser();
+
+var addNewUserButton = document.getElementById('addNewUser');
+var showPastUsersButton = document.getElementById('showPastUsers');
+var pastUsersForm = document.getElementById('pastUsers');
+var newUserForm = document.getElementById('newUser');
+
+console.log(newUserForm);
+
+addNewUserButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log(newUserForm.userNameSelected.value);
 });
+
+showPastUsersButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  console.log(pastUsersForm.userNameSelected.value);
+});
+
+
+
+
+
+
+
+
+
+// <button id="startPlaying">Play</button>
+// var playButton = document.getElementById('startPlaying');
+// playButton.addEventListener('click', function(event){
+//   var formShowing = document.getElementById(currentOptionShowing);
+//   console.log(formShowing);
+// });

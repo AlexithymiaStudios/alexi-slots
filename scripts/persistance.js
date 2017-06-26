d1dc450f889335b37a81c75c2f6e74192b596806
Tyler W <tyler.walser@yahoo.com>
2017-06-26 13:48:34 -0700

    added loadUser and getAllUsers functions

--- scripts/persistance.js
+++ scripts/persistance.js
@@ -26,8 +26,29 @@ Data.saveUser = function(user) {
   localStorage.setItem('users', JSON.stringify(usersToEdit));
 };
 
+Data.loadUser = function(user) {
+  var usersLoad =  localStorage.getItem('users');
 
+  if (usersLoad === null){
+    return null;
+  } else {
+    localStorage.setItem('currentUser', user.userName);
+    return JSON.parse(usersLoad)[user.userName];
+  };
+};
 
+Data.getAllUsers = function() {
+  var allUsersLoad = JSON.parse(localStorage.getItem('users'));
+  if (allUsersLoad === null) {
+    return [];
+  } else {
+    var temp = [];
+    for (var each in allUsersLoad) {
+      temp.append(allUsersLoad[each]);
+    }
+    return temp;
+  }
+};
 
 
 loadCurrentUser ();

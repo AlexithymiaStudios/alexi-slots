c2e0b25df1397e2742c67b6ec0abacbe10f7603d
Alana Franklin <alanafrankly@gmail.com>
2017-06-27 10:01:44 -0700

    Added basic styles to home page

---
+++ img/Emoji Slots_files/game-app.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var emojiArray = ['alien.png','cat.png','dancer.png','dog.png','poop.png','unicorn.png'];
+var currentEmojis = [];
+var playButton = document.getElementById('playButton');
+var image1 = document.getElementById('img1');
+var image2 = document.getElementById('img2');
+var image3 = document.getElementById('img3');
+var userNameSpan = document.getElementById('userName'); // Is this id name okay?
+var imageArray = [image1, image2, image3];
+
+// TODO: this is a fake function that we're using for testing. Delete before pushing.
+// var currentUser = {username: 'Quinn', rounds: 0, wins:0};
+
+// get data and name
+if (Data.loadCurrentUser() === null) {
+  //TODO - get persistance
+  // load settings page
+  window.location.href = 'options.html';
+  // Render username on page
+  renderUserName();
+} else {
+  var currentUser = Data.loadCurrentUser(); //TODO - get persistance
+  renderUserName();
+}
+
+// when play button is clicked
+playButton.addEventListener('click', function(event){
+  // picks a random img from emojiArray
+  currentEmojis = getRandomEmojis();
+  // update rounds in currentUser
+  currentUser.rounds++;
+  // update rendered image to DOM
+  renderEmojis();
+  // if three img are same
+  updateWins();
+  // update localStorage with new currentUser
+  Data.saveUser(currentUser); //TODO - get persistance
+  // get new data from localStorage
+  Data.loadCurrentUser(); //TODO - get persistance
+});
+
+// Function to get three random emoji images.
+function getRandomEmojis(){
+  var threeEmojis = [];
+  for (var i = 0; i < 3; i++) {
+    var random = Math.floor(Math.random() * emojiArray.length);
+    threeEmojis.push(emojiArray[random]);
+  }
+  return threeEmojis;
+}
+
+function renderEmojis(){
+  for (var i = 0; i < currentEmojis.length; i++) {
+    imageArray[i].setAttribute('src', 'img/' + currentEmojis[i]);
+    imageArray[i].setAttribute('alt', currentEmojis[i]);
+  }
+}
+
+function updateWins(){
+  if (currentEmojis[0] === currentEmojis[1] && currentEmojis[0] === currentEmojis[2]) {
+    // update wins in currentUser
+    currentUser.wins++;
+  }
+}
+
+function renderUserName(){
+  // update text content of the span
+  userNameSpan.textContent = currentUser.userName;
+}
---
+++ img/Emoji Slots_files/persistance.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function User(userName, rounds, wins) {
+  this.userName = userName;
+  this.rounds = rounds ? rounds : 0;
+  this.wins = wins ? wins : 0;
+}
+
+var Data = {};
+
+Data.loadCurrentUser = function() {
+  var currentUserLoad =  localStorage.getItem('currentUser');
+  var usersLoad = localStorage.getItem('users');
+  if (currentUserLoad === null || usersLoad === null){
+    return null;
+  } else {
+    return JSON.parse(usersLoad) [currentUserLoad];
+  };
+};
+
+Data.saveUser = function(user) {
+  localStorage.setItem('currentUser', user.userName);
+  var usersToEdit = JSON.parse(localStorage.getItem('users'));
+  if(usersToEdit === null) {
+    usersToEdit = {};
+  }
+  usersToEdit[user.userName] = user;
+  localStorage.setItem('users', JSON.stringify(usersToEdit));
+};
+
+Data.loadUser = function(user){
+  return this.loadUserName(user.userName);
+};
+
+Data.loadUserName = function(userName) {
+  var usersLoad =  localStorage.getItem('users');
+
+  if (usersLoad === null){
+    return null;
+  } else {
+    localStorage.setItem('currentUser', userName);
+    return JSON.parse(usersLoad)[userName];
+  };
+};
+
+Data.getAllUsers = function() {
+  var allUsersLoad = JSON.parse(localStorage.getItem('users'));
+  if (allUsersLoad === null) {
+    return [];
+  } else {
+    var temp = [];
+    for (var each in allUsersLoad) {
+      temp.push(allUsersLoad[each]);
+    }
+    return temp;
+  }
+};

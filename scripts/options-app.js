2b3dd3f9174080893cb29e3dd4df53ab5440cee4
ql24 <qlalberta@gmail.com>
2017-06-28 13:25:09 -0700

    finished most of the work to display balance

--- scripts/options-app.js
+++ scripts/options-app.js
@@ -2,6 +2,7 @@
 
 /************** VARIABLES **************/
 var resetButton = document.getElementById('resetBalanceButton');
+var select = document.createElement('select');
 
 function insertNewUser() {
   var form = document.createElement('form');
@@ -29,13 +30,14 @@ function insertPastUsers() {
   var button = document.createElement('button');
   form.setAttribute('id', 'pastUsers');
   label.textContent = 'Select the User';
-  var select = document.createElement('select');
   select.setAttribute('name', 'userNameSelected');
+  select.setAttribute('id', 'selectdPastUser')
   var allusers = Data.getAllUsers();
   for(var i = 0; i < allusers.length; i++){
     var each = allusers[i];
     var option = document.createElement('option');
     option.setAttribute('value', each.userName);
+    // option.setAttribute('class', 'option');// set class for option
     option.textContent = each.userName;
     select.appendChild(option);
   }
@@ -47,15 +49,28 @@ function insertPastUsers() {
   selectUser.appendChild(form);
 };
 
+function renderBalance(){
+//get allusers Data
+  var allUsersData = Data.getAllUsers();
+//get the value from the drop down list
+  var selectedUser = select[select.options.selectedIndex].value;
+  // console.log(selectedUser
+//pull out the user balance from the specific user out of the allusers Data
+  var selectedUserBalance = allUsersData[select.options.selectedIndex];
+  console.log(selectedUserBalance);
+// balanceElement.textContent = 'You balance is ' + currentUser.moneyBalance();
+}
+
 insertPastUsers();
 insertNewUser();
 
+var balanceElement = document.getElementById('selectdPastUser');
 var addNewUserButton = document.getElementById('addNewUser');
 var showPastUsersButton = document.getElementById('showPastUsers');
 var pastUsersForm = document.getElementById('pastUsers');
 var newUserForm = document.getElementById('newUser');
 
-console.log(newUserForm);
+// console.log(newUserForm);
 
 addNewUserButton.addEventListener('click', function(event) {
   event.preventDefault();
@@ -72,11 +87,31 @@ showPastUsersButton.addEventListener('click', function(event) {
   var userSelected = Data.loadUserName(pastUsersForm.userNameSelected.value);
   if (userSelected !== null ) {
     Data.saveUser(userSelected);
+    // renderBalance();
     window.location.href = 'index.html';
   } else {
     console.warn('Was not able to load selected user');
   }
 });
+
+// var option = option.setAttribute('class', 'option');
+// var option = document.getElementsByClassName('option')
+// option.addEventListener ('click', function() {
+//   renderBalance();
+// });
+
+// var option = option.setAttribute('class', 'option');
+// var option = document.getElementsByClassName('option')
+// s.addEventListener ('click', function() {
+//   renderBalance();
+// });
+
+
+balanceElement.onchange = function () {
+  var elem = (typeof this.selectedIndex === 'undefined' ? window.event.srcElement : this);
+  var value = elem.value || elem.options[elem.selectedIndex].value;
+  alert(value);
+};
 
 function checkIfValid(formInput) {
   var validUsername = formInput.value.match(/^[a-zA-Z]+$/);

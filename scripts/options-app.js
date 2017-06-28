d965d336559078703f3b01675a6e95c739b8401c
Alana Franklin <alanafrankly@gmail.com>
2017-06-28 15:03:27 -0700

    Reorganized stats app

--- scripts/options-app.js
+++ scripts/options-app.js
@@ -1,19 +1,19 @@
 'use strict';
 
-/************** VARIABLES **************/
+/*********** VARIABLE ***********/
 var resetButton = document.getElementById('resetBalanceButton');
 
-/************** SETUP **************/
+/*********** SETUP ***********/
 insertPastUsers();
 insertNewUser();
 
-/************** ADDITIONAL VARIABLES **************/
+/*********** ADDITIONAL VARIABLES ***********/
 var addNewUserButton = document.getElementById('addNewUser');
 var showPastUsersButton = document.getElementById('showPastUsers');
 var pastUsersForm = document.getElementById('pastUsers');
 var newUserForm = document.getElementById('newUser');
 
-/************** FUNCTIONS **************/
+/*********** FUNCTIONS ***********/
 function insertNewUser() {
   var form = document.createElement('form');
   var label = document.createElement('label');
@@ -68,7 +68,7 @@ function checkIfValid(formInput) {
   return true;
 };
 
-/************** EVENT LISTENERS **************/
+/*********** EVENT LISTENERS ***********/
 addNewUserButton.addEventListener('click', function(event) {
   event.preventDefault();
   if (checkIfValid(newUserForm.userNameSelected)){
--- scripts/stats-app.js
+++ scripts/stats-app.js
@@ -1,53 +1,68 @@
 'use strict';
 
+/*********** VARIABLES ***********/
 var allData = Data.getAllUsers();
 var chartParent = document.getElementById('chart');
-
 var userNames = [];
 var losses = [];
 var jackpots = [];
 var pairs = [];
 
-// get data and name
-if (Data.loadCurrentUser() === null) {
-  window.location.href = 'options.html';
+/*********** SETUP***********/
+function setup(){
+  // if no users are defined, switch to options page
+  if (Data.loadCurrentUser() === null) {
+    window.location.href = 'options.html';
+  }
+  setUpGraphData();
+  drawGraph();
 }
 
-for (var i in allData){
-  userNames.push(allData[i].userName);
-  losses.push(allData[i].losses());
-  jackpots.push(allData[i].jackpots);
-  pairs.push(allData[i].pairs);
+setup();
+/*********** FUNCTIONS ***********/
+
+function setUpGraphData(){
+  for (var i in allData){
+    userNames.push(allData[i].userName);
+    losses.push(allData[i].losses());
+    jackpots.push(allData[i].jackpots);
+    pairs.push(allData[i].pairs);
+  }
 }
 
-for (var j = 0; j < userNames.length; j++){
+function drawGraph(){
+  //for each user
+  for (var j = 0; j < userNames.length; j++){
+    // create the data set
+    var data = {
+      datasets: [{
+        data: [losses[j],jackpots[j], pairs[j]],
+        backgroundColor: ['red', 'blue', 'green']
+      }],
+      labels: [
+        'Losses',
+        'Jackpots',
+        'Pairs'
+      ]
+    };
 
-  var data = {
-    datasets: [{
-      data: [losses[j],jackpots[j], pairs[j]],
-      backgroundColor: ['red', 'blue', 'green']
-    }],
-    labels: [
-      'Losses',
-      'Jackpots',
-      'Pairs'
-    ]
-  };
-  var userDiv = document.createElement('div');
-  userDiv.setAttribute('class', 'column col-5 userChart');
-  var h3 = document.createElement('h3');
-  userDiv.append(h3);
-  var canvas = document.createElement('canvas');
-  h3.textContent = userNames[j];
+    // create elements for the user chart and append them to the window
+    var userDiv = document.createElement('div');
+    userDiv.setAttribute('class', 'column col-5 userChart');
+    var h3 = document.createElement('h3');
+    userDiv.append(h3);
+    var canvas = document.createElement('canvas');
+    h3.textContent = userNames[j];
 
-  userDiv.append(h3);
-  userDiv.append(canvas);
-  chartParent.append(userDiv);
-  var ctx = canvas.getContext('2d');
+    userDiv.append(h3);
+    userDiv.append(canvas);
+    chartParent.append(userDiv);
+    var ctx = canvas.getContext('2d');
 
-  // For a pie chart
-  new Chart(ctx,{
-    type: 'pie',
-    data: data
-  });
+    // Draw the actual chart
+    new Chart(ctx,{
+      type: 'pie',
+      data: data
+    });
+  }
 }

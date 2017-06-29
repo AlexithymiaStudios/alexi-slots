0d550a62b05b03c0d7e96fe701aed1fbae637485
Alana Franklin <alanafrankly@gmail.com>
2017-06-29 11:17:38 -0700

    Added additional emoji files

--- scripts/persistance.js
+++ scripts/persistance.js
@@ -5,7 +5,13 @@ var initialBalance = 25;
 var jackpotWorth = 5;
 var pairWorth = 2;
 var Data = {};
-var slots = {standard:['alien.png','cat.png','dancer.png','dog.png','poop.png','unicorn.png']};
+var slots = {standard:['alien.png','cat.png','dancer.png','dog.png','poop.png','unicorn.png'],
+  underWater:['whale.png','dolphin.png','blowfish.png','fish.png','tropical_fish.png', 'octopus.png'],
+  fruit: ['strawberry.png', 'grapes.png','cherries.png','pineapple.png','kiwi.png','tangerine.png'],
+  animals: ['fox.png','turtle.png','turkey.png','elephant.png','hatched_chick.png','butterfly.png'],
+  sports: ['soccer.png','basketball.png','football.png','8ball.png','ping-pong.png','ski.png'],
+  faces: ['heart_eyes.png','relaxed.png','rofl.png','pensive.png','triumph.png','scream.png']
+};
 
 /*********** USER CONSTRUCTOR ***********/
 function User(userName) {
